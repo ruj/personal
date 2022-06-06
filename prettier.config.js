65060a4ac61cbe6c122aa5b5335f6a568f7be89c
ruj <214782@bk.ru>
2022-06-06 17:51:56 -0300

    style: linter

--- prettier.config.js
+++ prettier.config.js
@@ -1,6 +1,14 @@
 module.exports = {
+  plugins: [require.resolve('@prettier/plugin-pug')],
   semi: false,
   singleQuote: true,
   trailingComma: 'none',
-  endOfLine: 'auto'
+  endOfLine: 'auto',
+  pugBracketSpacing: false,
+  pugSemi: false,
+  pugSingleQuote: false,
+  pugAttributeSeparator: 'none',
+  pugCommentPreserveSpaces: 'trim-all',
+  pugWrapAttributesThreshold: 1,
+  pugExplicitDiv: true
 }
